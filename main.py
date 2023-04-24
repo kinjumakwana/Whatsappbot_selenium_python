8f3aa17395ad2c4144e0d01a44596a8bb8d5d72d
Kinjal Makwana <109803739+kinjumakwana@users.noreply.github.com>
2023-04-24 11:24:22 +0530

    No QR Code | Python Selenium Web Automation

--- main.py
+++ main.py
@@ -9,35 +9,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from django.db import IntegrityError
 from selenium.webdriver.common.action_chains import ActionChains
-
+import pickle
 import re 
 import pandas as pd
 import csv
-
+import os
+CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\BAPS\\AppData\\Local\\Google\\Chrome\\User Data\\Wtsp"
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-blink-features=AutomationControlled')
-# options.add_argument(r'user-data-dir=C:\Users\BAPS\AppData\Local\Google\Chrome\User Data')
-# options.add_argument('--profile-directory=Default')
+options.add_argument(CHROME_PROFILE_PATH)
+options.add_argument('--profile-directory=Default')
 
 # C:\Users\BAPS\AppData\Local\Google\Chrome\User Data
 s = Service(r"D:\Kinjal\chromedriver_win32\chromedriver.exe")
-# driver = webdriver.Chrome(service=s, options=options)
 
 class Whatsappbot:
     def __init__(self):
         self.driver = webdriver.Chrome(service=s, options=options)
+        self.driver.maximize_window()
         self.driver.get("https://web.whatsapp.com/")
-        # sleep(5)
-        # wait = WebDriverWait(self.driver, 5)
+        WebDriverWait(self.driver, 5)
+        print("Logged in successfully")
         
     def whatsappdata(self):
-        data={}
-        Blog_dict={}
         try:
-            self.driver.maximize_window()
-            self.driver.get("https://web.whatsapp.com/")
             sleep(10)
-            
             # input("Press enter after scanning QR code")
             targets = ['"Ritesh Patel"','"Dineshbhai@aahaa"']
             message="Hello testing"
@@ -47,32 +43,32 @@ class Whatsappbot:
                     contact_path='//span[contains(@title,'+ target +')]'
                     # contact_path='//span[contains(@title, {})]'.format(str(target))
                     
-                    contact=WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH,contact_path)))
+                    contact=WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,contact_path)))
                     contact.click()
-                    sleep(5)
+                    # sleep(5)
                     message_box_path='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
-                    message_box=WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH,message_box_path)))
+                    message_box=WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,message_box_path)))
                     
                     message_box.send_keys(message + Keys.ENTER)
-                    sleep(10)
+                    sleep(5)
                 
                 except Exception as e:
-                    print(f"Could not send message to {target}.")  
+                    # print(f"Could not send message to {target}.")  
                     #   print(e)  
                     search_bar_path = '/html/body/div[1]/div/div/div[4]/div/div[1]/div/div/div[2]/div/div[1]'
                     search_bar = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, search_bar_path)))
                     search_bar.click()
                     target1 = target.strip()
-                    print(target1)
+                    # print(target1)
                     target2 = target1.strip().replace('"', '')
-                    print(target2)
+                    # print(target2)
                     search_bar.send_keys(target2 + Keys.ENTER)
                     sleep(2)
                     message_box_path='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
-                    message_box=WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH,message_box_path)))
-                    
+                    message_box=WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH,message_box_path)))
                     message_box.send_keys(message + Keys.ENTER)
-                    sleep(10)
+                    sleep(5)
+                    # WebDriverWait(self.driver, 10)
                 
                                 
         except Exception as e:
